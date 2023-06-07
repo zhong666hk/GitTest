67c7a2f86f0858af3c0b74ae49dfa1a4b685f44e
George <1448721696@qq.com>
2023-06-07 15:37:55 +0800

    third commit

--- src/com/wbu/Gittest.java
+++ src/com/wbu/Gittest.java
@@ -4,6 +4,7 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("第二个版本来了!!");
-        System.out.println("我是hot-fix分支");
+        System.out.println("我是master分支");
+        System.out.println("冲突来喽");
     }
 }
