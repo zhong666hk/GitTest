9361b9c0083676b9b9f81c11db6ada69b0ca3636
George <1448721696@qq.com>
2023-06-07 15:32:16 +0800

    second commit

--- src/com/wbu/Gittest.java
+++ src/com/wbu/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("第二个版本来了!!");
+        System.out.println("我是hot-fix分支");
     }
 }
